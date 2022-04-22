d15fb31f667546ccc2f5d35364f11709f61a558f
mLochness <lochness@sernato.sk>
2022-04-23 01:05:27 +0200

    slide name attribute added

--- pageslide.js
+++ pageslide.js
@@ -1,26 +1,27 @@
-//click or touch page to next fullscreen
 $(document).ready(function () {
-  $(".slideNum").on("click tap touch touchstart", function (event) {
-    event.preventDefault();
 
-    if ($(".current").next().length) {
-      var nextPage = $(".current").next(),
-        section = $(this).closest(".wrapper");
-      section.animate(
-        {
-          scrollTop: section.scrollTop() + nextPage.offset().top
-        },
-        300
-      );
-      $(".current").removeClass("current");
-      $(nextPage).addClass("current");
-      checkcurrent();
-    }
-  });
-});
+  //click or touch page to next fullscreen
+  $(".slideNum")
+    .off("click tap touch touchstart")
+    .on("click tap touch touchstart", function (event) {
+      event.preventDefault();
 
-//upDown bool true = down ... false = up
-$(document).ready(function () {
+      if ($(".current").next().length) {
+        var nextPage = $(".current").next(),
+          section = $(this).closest(".wrapper");
+        section.animate(
+          {
+            scrollTop: section.scrollTop() + nextPage.offset().top
+          },
+          300
+        );
+        $(".current").removeClass("current");
+        $(nextPage).addClass("current");
+        checkcurrent();
+      }
+    });
+
+  //upDown bool true = down ... false = up
   function move(current, upDown) {
     if (upDown) {
       if ($(current).next().length) {
@@ -60,7 +61,6 @@ $(document).ready(function () {
       e.originalEvent.detail > 1
     ) {
       move($(".current"), true);
-      checkcurrent();
     }
 
     if (
@@ -70,100 +70,102 @@ $(document).ready(function () {
       -e.originalEvent.detail < -1
     ) {
       move($(".current"), false);
-      checkcurrent();
     }
   });
-});
 
-// scroll to top button
-$(document).ready(function () {
+  // scroll to top button
   $(".backtop-btn").click(function () {
     $(".wrapper").animate({ scrollTop: 0 }, 800);
     $(".page").removeClass("current");
     $(".page").first().addClass("current");
     checkcurrent();
   });
-});
 
-// fired wheel/touchmove or swipe events counter
-var triggers = 0;
-$(document).on("wheel", function (e) {
-  $(".count").text(++triggers);
-});
+  // fired wheel/touchmove or swipe events counter
+  var triggers = 0;
+  $(document).on("wheel", function (e) {
+    $(".count").text(++triggers);
+  });
 
-$(document).on("touchmove swipe", function () {
-  $(document).trigger("wheel");
-});
+  $(document).on("touchmove touchstart swipe", function () {
+    $(document).trigger("wheel");
+  });
 
-//page navigation nodes generator
-$(document).ready(function () {
+  //page navigation nodes generator
   $(".page").each(function () {
     var pageid = $(this).attr("id");
     var pagename = $(this).attr("name");
-    var slink = $(
-      "<a class='page-nav-bull'><span class='slinkdesc'>" +
-        pagename +
-        "</span></a>"
+    var plink = $(
+      "<a class='page-nav-bull'><span class='plinkdesc'>" +
+      pagename +
+      "</span></a>"
     ).attr("href", "#" + pageid);
-    $(".pageNav").append(slink);
+    $(".pageNav").append(plink);
     $(".pageNav a").first().addClass("current-bull");
-
   });
-});
 
-//assign current class for nav bullets
-function checkcurrent() {
-  $(".pageNav a").each(function () {
-    var bullhref = $(this).attr("href");
-    if (bullhref == "#" + $(".current").attr("id")) {
-      $(".pageNav a").removeClass("current-bull");
-      $(this).addClass("current-bull");
-      console.log(
-        bullhref,
-        "=",
-        "#" + $(".current").attr("id"),
-        bullhref == "#" + $(".current").attr("id"),
-        "checkcurrent fired"
-      );
-    }
-  });
-}
+  //assign current class for page nav bullets
+  function checkcurrent() {
+    $(".pageNav a").each(function () {
+      var bullhref = $(this).attr("href");
+      if (bullhref == "#" + $(".current").attr("id")) {
+        $(".pageNav a").removeClass("current-bull");
+        $(this).addClass("current-bull");
+        console.log(
+          bullhref,
+          "=",
+          "#" + $(".current").attr("id"),
+          bullhref == "#" + $(".current").attr("id"),
+          "checkcurrent fired"
+        );
+      }
+    });
+  }
 
-//bullet navigation - assign current page class
-$(document).ready(function () {
+  //bullet navigation - assign current page class
   $(".pageNav a").click(function () {
     var bullhref = $(this).attr("href");
     $(".current").removeClass("current");
     $(bullhref).addClass("current");
     checkcurrent();
   });
-});
 
-// slide to side
-function slideLeft() {
-  var prevSlide = $(".current .curSlide").prev();
-  if ($(prevSlide).length) {
-    $(".current .slideDiv").animate({ left: "+=100vw" }, 300);
-    $(".curSlide").removeClass("curSlide");
-    $(prevSlide).addClass("curSlide");
-  } else {
-    $(".current .curSlide").effect("shake", { times: 1 }, 200);
+  // slide to side
+  function slideLeft() {
+    var prevSlide = $(".current .curSlide").prev();
+    if ($(prevSlide).length) {
+      $(".current .slideDiv").animate({ "left": "+=100vw" }, 300);
+      $(".current .curSlide").removeClass("curSlide");
+      $(prevSlide).addClass("curSlide");
+      checkCurBul();
+    } else {
+      $(".current .curSlide").effect("shake", { times: 1 }, 200);
+    }
   }
-}
 
-function slideRight() {
-  var nextSlide = $(".current .curSlide").next();
-  if ($(nextSlide).length) {
-    $(".current .slideDiv").animate({ left: "-=100vw" }, 300);
-    $(".curSlide").removeClass("curSlide");
-    $(nextSlide).addClass("curSlide");
-  } else {
-    $(".current .curSlide").effect("shake", { times: 1 }, 200);
+  function slideRight() {
+    var nextSlide = $(".current .curSlide").next();
+    if ($(nextSlide).length) {
+      $(".current .slideDiv").animate({ "left": "-=100vw" }, 300);
+      $(".current .curSlide").removeClass("curSlide");
+      $(nextSlide).addClass("curSlide");
+      checkCurBul();
+    } else {
+      $(".current .curSlide").effect("shake", { times: 1 }, 200);
+    }
   }
-}
 
-$(document).ready(function () {
-  $(".spslide").first().addClass("curSlide");
+  //get slideDiv width from No. of slides
+  $(".slideDiv").each(function () {
+    var slideCount = $(this).children().length;
+    var autoWidth = $(this);
+    autoWidth.css({ width: autoWidth.width() * slideCount });
+    console.log($(this).parent().attr("id"), "slides:", slideCount);
+  });
+
+  $(".slideDiv").each(function () {
+    $(this).find(".spslide").first().addClass("curSlide");
+  });
   $(".slideNavL").click(function () {
     slideLeft();
     console.log("left arrow Nav");
@@ -185,4 +187,42 @@ $(document).ready(function () {
       console.log("right arrow key");
     }
   });
+
+  //slide navigation nodes generator
+  $(".spslide").each(function () {
+    var slideid = $(this).attr("id");
+    var slidename = $(this).attr("name");
+    //add name attribute to bullet:
+    var slink = $("<a class='slide-nav-bull'><span class='slinkdesc'>" + slidename + "</span></a>").attr("name", slideid);
+    $(this).closest(".page").find(".slideBul").append(slink);
+    $(this).closest(".page").find(".slideBul a").first().addClass("current-bull");
+  });
+
+  //assign current class for slide nav bullets
+  function checkCurBul() {
+    $(".slideBul a").each(function () {
+      var bullhref = $(this).attr("name");
+      if (bullhref == $(".current .curSlide").attr("id")) {
+        $(".current .slideBul a").removeClass("current-bull");
+        $(this).addClass("current-bull");
+        console.log(bullhref, "=", $(".current .curSlide").attr("id"), "checkCurBul fired");
+      }
+    });
+  }
+
+  //slide bullet click function
+  $(".slideBul a").each(function () {
+    var sBulIndex = $(this).index();
+    var slideOffset = (sBulIndex * -100) + "vw";
+    $(this).click(function () {
+      $(".current .slideDiv").animate({ "left": slideOffset }, 300);
+      $(".current .curSlide").removeClass("curSlide");
+      $(".current .spslide:eq(" + sBulIndex + ")").addClass("curSlide");
+      console.log("sBulIndex =", sBulIndex, "slideOffset =", slideOffset);
+      checkCurBul();
+    });
+  });
+
+
+
 });
