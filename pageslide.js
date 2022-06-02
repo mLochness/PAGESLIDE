225fb7241e9fcc6bf83b41c5bc6bb8a2c57eef23
mLochness <lochness@sernato.sk>
2022-06-02 23:24:42 +0200

    wheel and touchmove events debug in progress

--- pageslide.js
+++ pageslide.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-
-  //click or touch element to next fullscreen
+ 
+  //prep for drag or touch move firing scroll to next/prev page/slide
+  $('.wrapper').draggable({axis: "y", revert: true});
+  $('.slideDiv').draggable({axis: "x", revert: true});
+  
+ //click or touch element to next fullscreen
   $(".slideNum")
-    .on("click", function () {
-      if ($(".current").next().length) {
+  .on("click", function () {
+     if ($(".current").next().length) {
         var nextPage = $(".current").next(),
           section = $(this).closest(".wrapper");
         section.animate(
@@ -19,35 +23,39 @@ $(document).ready(function () {
     });
 
 
-  //mobile touchmove Y  
+ //mobile touchmove Y  
   var lastY;
-  $(document).on('touchmove', function (e) {
-    e.preventDefault();
-    var currentY = e.originalEvent.touches[0].clientY;
-    if (currentY > lastY + 60) {
-      move($(".current"), true);
-      console.log("touchDown");
-    } else if (currentY < lastY - 60) {
-      move($(".current"), false);
-      console.log("touchUp");
+$(document).on('touchmove', function (e){
+  //e.preventDefault();
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+     var currentY = e.originalEvent.touches[0].clientY;
+     if(currentY > lastY+60){
+         move($(".current"), true);
+       console.log("touchDown");
+     }else if(currentY < lastY-60){
+         move($(".current"), false);
+       console.log("touchUp");
+     }
+     lastY = currentY;
     }
-    lastY = currentY;
-  });
+});
 
-  //mobile touchmove X
+//mobile touchmove X
   var lastX;
-  $(document).on('touchmove', function (e) {
-    e.preventDefault();
-    var currentX = e.originalEvent.touches[0].clientX;
-    if (currentX > lastX + 60) {
-      slideRight();
-      console.log("swipeRight");
-    } else if (currentX < lastX - 60) {
-      slideLeft();
-      console.log("swipeLeft");
+$(document).on('touchmove', function (e){
+  e.preventDefault();
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+     var currentX = e.originalEvent.touches[0].clientX;
+     if(currentX > lastX+60){
+         slideRight();
+       console.log("swipeRight");
+     }else if(currentX < lastX-60){
+         slideLeft();
+       console.log("swipeLeft");
+     }
+     lastX = currentX;
     }
-    lastX = currentX;
-  });
+});
 
   //upDown bool true = down ... false = up
   function move(current, upDown) {
@@ -83,34 +91,30 @@ $(document).ready(function () {
   $(".page").first().addClass("current");
   $(document).on("keydown", function (e) {
     if (
-      e.keyCode === 34 ||
-      e.keyCode === 40
-      //  || event.originalEvent.deltaY > 1  // conflict with touchmove
+      e.keyCode === 34 || e.keyCode === 40 
     ) {
       move($(".current"), true);
     }
 
     if (
-      e.keyCode === 33 ||
-      e.keyCode === 38
-      //  || event.originalEvent.deltaY < -1
+      e.keyCode === 33 || e.keyCode === 38
     ) {
       move($(".current"), false);
     }
   });
-
+ 
   // scroll to next button
   $(".next-btn").click(function (e) {
-    e.preventDefault();
-    if ($(".current").next().length) {
-      var nextPage = $(".current").next(),
+    e.preventDefault(); 
+    if($(".current").next().length) {
+    var nextPage = $(".current").next(),
         section = $(".current").closest(".wrapper");
-      section.animate({ scrollTop: section.scrollTop() + nextPage.offset().top }, 300);
+    section.animate({scrollTop: section.scrollTop() + nextPage.offset().top }, 300);
       $(".current").removeClass("current");
       $(nextPage).addClass("current");
       checkcurrent();
       console.log("next Button");
-    }
+   }
   });
   // scroll to top button
   $(".backtop-btn").click(function () {
@@ -126,8 +130,8 @@ $(document).ready(function () {
     var pagename = $(this).attr("name");
     var plink = $(
       "<a class='page-nav-bull'><span class='plinkdesc'>" +
-      pagename +
-      "</span></a>"
+        pagename +
+        "</span></a>"
     ).attr("href", "#" + pageid);
     $(".pageNav").append(plink);
     $(".pageNav a").first().addClass("current-bull");
@@ -155,7 +159,7 @@ $(document).ready(function () {
   //bullet navigation - assign current page class
   $(".pageNav a").click(function () {
     var bullhref = $(this).attr("href");
-    $(".current").removeClass("current");
+   $(".current").removeClass("current");
     $(bullhref).addClass("current");
     checkcurrent();
     console.log(bullhref);
@@ -194,15 +198,15 @@ $(document).ready(function () {
     var slideCount = $(this).children().length;
     var autoWidth = $(this);
     autoWidth.css({ width: autoWidth.width() * slideCount });
-    console.log($(this).parent().attr("id"), "slides:", slideCount);
+    console.log($(this).parent().attr("id"),"slides:", slideCount);
   });
-
+   
   //get offset +100vw for each slide from index
   $(".spslide").each(function () {
     var slideIndex = $(this).index();
-    $(this).css({ "left": (100 * slideIndex) + "vw" });
+    $(this).css({"left" : (100*slideIndex) +"vw"});
   });
-
+  
   $(".slideDiv").each(function () {
     $(this).find(".spslide").first().addClass("curSlide");
   });
@@ -215,62 +219,78 @@ $(document).ready(function () {
     console.log("right arrow Nav");
   });
 
-  $(document).on("keydown touchmove swipe", function (e) {
-    if (e.keyCode === 37 || e.originalEvent.deltaX < 1) {
+  //wheel for mouse with X-axis scrolling
+  $(document).on("keydown wheel", function (e) {
+    if (e.keyCode === 37 || e.originalEvent.deltaX > 1 && triggers > 5) {
       slideLeft();
+      triggers = 0;
       console.log("left arrow key");
     }
   });
-  $(document).on("keydown touchmove swipe", function (e) {
-    if (e.keyCode === 39 || e.originalEvent.deltaX < -1) {
+  $(document).on("keydown wheel", function (e) {
+    if (e.keyCode === 39 || e.originalEvent.deltaX < -1 && triggers > 5) {
       slideRight();
+      triggers = 0;
       console.log("right arrow key");
     }
   });
-
-  //slide navigation nodes generator
-  $(".spslide").each(function () {
+  
+    //slide navigation nodes generator
+     $(".spslide").each(function() {
     var slideid = $(this).attr("id");
     var slidename = $(this).attr("name");
-
-    //add name attribute to bullet:
-    var slink = $("<a class='slide-nav-bull'><span class='slinkdesc'>" + slidename + "</span></a>").attr("name", slideid);
+ 
+     //add name attribute to bullet:
+    var slink = $("<a class='slide-nav-bull'><span class='slinkdesc'>" + slidename + "</span></a>" ).attr("name", slideid);
     $(this).closest(".page").find(".slideBul").append(slink);
     $(this).closest(".page").find(".slideBul a").first().addClass("current-bull");
-  });
-
-  //assign current class for slide nav bullets
-  function checkCurBul() {
+      });
+  
+    //assign current class for slide nav bullets
+    function checkCurBul() {
     $(".slideBul a").each(function () {
       var bullhref = $(this).attr("name");
       if (bullhref == $(".current .curSlide").attr("id")) {
         $(".current .slideBul a").removeClass("current-bull");
         $(this).addClass("current-bull");
-        console.log(bullhref, "=", $(".current .curSlide").attr("id"), "checkCurBul fired");
+        console.log( bullhref, "=", $(".current .curSlide").attr("id"), "checkCurBul fired");
       }
     });
   }
+  
 
   //slide bullet click function
-  $(".slideBul a").each(function () {
-    var sBulIndex = $(this).index();
-    var slideOffset = (sBulIndex * -100) + "vw";
-    $(this).click(function () {
-      $(".current .slideDiv").animate({ "left": slideOffset }, 300);
-      $(".current .curSlide").removeClass("curSlide");
-      $(".current .spslide:eq(" + sBulIndex + ")").addClass("curSlide");
-      console.log("sBulIndex =", sBulIndex, "slideOffset =", slideOffset);
-      checkCurBul();
-    });
+    $(".slideBul a").each(function(){
+      var sBulIndex = $(this).index();
+      var slideOffset = (sBulIndex*-100)+"vw";
+        $(this).click(function(){
+        $(".current .slideDiv").animate({ "left": slideOffset }, 300);
+        $(".current .curSlide").removeClass("curSlide");
+        $(".current .spslide:eq("+sBulIndex+")").addClass("curSlide");
+        console.log("sBulIndex =", sBulIndex, "slideOffset =", slideOffset);
+        checkCurBul();
+     });
+   });
+  
+  
+   // fired wheel/touchmove or swipe events counter
+   // limit multiple wheel triggers to one move - not solved yet
+  var triggers = 0;
+  $(document).on("wheel touchmove", function (e) {
+   $(".count").text(++triggers);
+    if (
+     e.originalEvent.deltaY > 1 && triggers > 25
+    ) {
+      move($(".current"), true);
+      triggers = 0;
+    }
+    if (
+     e.originalEvent.deltaY < -1 && triggers > 25
+    ) {
+      move($(".current"), false);
+      triggers = 0;
+    }
   });
-
-});
-
-// fired wheel/touchmove or swipe events counter
-var triggers = 0;
-$(document).on("wheel", function (e) {
-  $(".count").text(++triggers);
-});
-$(document).on("touchmove swipe", function () {
-  $(document).trigger("wheel");
+  
+    
 });
